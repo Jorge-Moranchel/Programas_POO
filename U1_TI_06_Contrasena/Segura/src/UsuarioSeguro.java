aac1ae3bffec9754f0fa1878ce6b13334b1038dc
Jorge-Moranchel <20243ds059@utez.edu.mx>
2025-06-04 07:50:58 -0600

    Ya hice los if para validar que solo sean caracteres menores a 8

--- U1_TI_06_Contrasena/Segura/src/UsuarioSeguro.java
+++ U1_TI_06_Contrasena/Segura/src/UsuarioSeguro.java
@@ -1,12 +1,29 @@
 public class UsuarioSeguro {
 
-    String nombreUsuario;
-    String password;
+    private String nombreUsuario;
+    private String password;
 
     public void setNombreUsuario(String nombreUsuario) {
-        this.nombreUsuario = nombreUsuario;
+        if (nombre != null && !nombreUsuario.isEmpty()){
+            this.nombreUsuario = nombreUsuario;
+        }else {
+            System.out.println("El nombre no puede ser vacio");
+        }
+
     }
     public void setPassword(String password) {
-        this.password = password;
+        if (password != null || !password.isEmpty()){
+            System.out.println("El password no puede ser vacio");
+            return;
+        }
+        if (nuevaPassword.length() < 8) {
+            System.out.println("La contraseña debe tener al menos 8 caracteres.");
+            return;
+        }
+        boolean tieneMayuscula = false;
+        boolean tieneMinuscula = false;
+        boolean tieneDigito = false;
+
+
     }
 }
