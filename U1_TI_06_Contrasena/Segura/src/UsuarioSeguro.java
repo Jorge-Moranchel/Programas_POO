d2afd7ca9ef083f69ccdf05788186390164e118d
Jorge-Moranchel <20243ds059@utez.edu.mx>
2025-06-04 07:44:44 -0600

    Ya hice los setters de los private

---
+++ U1_TI_06_Contrasena/Segura/src/UsuarioSeguro.java
@@ -0,0 +1,12 @@
+public class UsuarioSeguro {
+
+    String nombreUsuario;
+    String password;
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
