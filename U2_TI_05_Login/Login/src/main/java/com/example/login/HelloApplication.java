1040e7582d630f3a961a1109ca706b25e24f5bad
Jorge-Moranchel <20243ds059@utez.edu.mx>
2025-06-13 08:57:11 -0600

    Vamos bien creando los logins

--- U2_TI_05_Login/Login/src/main/java/com/example/login/HelloApplication.java
+++ U2_TI_05_Login/Login/src/main/java/com/example/login/HelloApplication.java
@@ -1,8 +1,9 @@
 package com.example.login;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,7 +11,15 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        
+        TextField txtNombre = new TextField();
+        TextField txtApellido = new TextField();
+        TextField txtEdad = new TextField();
+
+        ComboBox<String> comboRol = new ComboBox<>();
+        comboRol.getItems().addAll("Estudiante", "Profesor", "Admin");
+
+        Button btnCrear = new Button("Crear");
+        Button btnReset = new Button("Reset");
     }
 
     public static void main(String[] args) {
