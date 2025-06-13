2e392a15aea4e781a38ed499110f149cf10e5122
Jorge-Moranchel <20243ds059@utez.edu.mx>
2025-06-13 09:08:22 -0600

    Pues copiamos un codigo que tenia antes

--- U2_TI_05_Login/Login/src/main/java/com/example/login/HelloApplication.java
+++ U2_TI_05_Login/Login/src/main/java/com/example/login/HelloApplication.java
@@ -1,9 +1,12 @@
 package com.example.login;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.layout.*;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,15 +14,19 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        TextField txtNombre = new TextField();
-        TextField txtApellido = new TextField();
-        TextField txtEdad = new TextField();
-
-        ComboBox<String> comboRol = new ComboBox<>();
-        comboRol.getItems().addAll("Estudiante", "Profesor", "Admin");
-
+        Label lblNombre = new Label("Nombre");
+        TextField tfNombre = new TextField();
+        Label lblApellido = new Label("Apellido");
+        TextField tfApellido = new TextField();
+        Label lblEdad = new Label("Edad");
+        TextField tfEdad = new TextField();
+        Label lblTipo = new Label("Tipo");
+        ObservableList<String> tipos = FXCollections.observableArrayList("estudiante", "profesor", "admin");
+        ComboBox<String> cbTipo = new ComboBox<>(tipos);
         Button btnCrear = new Button("Crear");
         Button btnReset = new Button("Reset");
+        Label lblResultado = new Label();
+
     }
 
     public static void main(String[] args) {
