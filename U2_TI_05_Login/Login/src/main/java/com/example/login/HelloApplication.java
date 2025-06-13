71685c900594312a3bc8485eb237533659ea34e3
Jorge-Moranchel <20243ds059@utez.edu.mx>
2025-06-13 09:32:47 -0600

    Sobres ya quedo creo yo xdddd, vengo de HTML Y CSS xd

--- U2_TI_05_Login/Login/src/main/java/com/example/login/HelloApplication.java
+++ U2_TI_05_Login/Login/src/main/java/com/example/login/HelloApplication.java
@@ -6,6 +6,7 @@ import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -21,11 +22,63 @@ public class HelloApplication extends Application {
         Label lblEdad = new Label("Edad");
         TextField tfEdad = new TextField();
         Label lblTipo = new Label("Tipo");
-        ObservableList<String> tipos = FXCollections.observableArrayList("estudiante", "profesor", "admin");
+        ObservableList<String> tipos = FXCollections.observableArrayList("Estudiante", "Profesor", "Adminzzz");
         ComboBox<String> cbTipo = new ComboBox<>(tipos);
         Button btnCrear = new Button("Crear");
         Button btnReset = new Button("Reset");
         Label lblResultado = new Label();
+
+        GridPane root = new GridPane();
+        root.setAlignment(Pos.CENTER);
+        root.setHgap(10);
+        root.setVgap(10);
+
+        root.add(lblNombre, 0, 0);
+        root.add(tfNombre, 1, 0);
+        root.add(lblApellido, 0, 1);
+        root.add(tfApellido, 1, 1);
+        root.add(lblEdad, 0, 2);
+        root.add(tfEdad, 1, 2);
+        root.add(lblTipo, 0, 3);
+        root.add(cbTipo, 1, 3);
+        root.add(btnCrear, 0, 4);
+        root.add(btnReset, 1, 4);
+        root.add(lblResultado, 0, 5, 2, 1);
+
+        btnCrear.setOnAction(e -> {
+           String nombre = tfNombre.getText();
+           String apellido = tfApellido.getText();
+           String edad = tfEdad.getText();
+           String tipo = cbTipo.getSelectionModel().getSelectedItem();
+
+
+        if (nombre.isEmpty() || apellido.isEmpty() || edad.isEmpty() || tipo.isEmpty()) {
+            lblResultado.setText("Por favor informe todos los campos");
+            lblResultado.setStyle("-fx-background-color: #936060;");
+            root.setStyle("-fx-background-color: #babda4;");
+        }else {
+            lblResultado.setText("Nombre: " + nombre + " | Apellido: " + apellido + " | Edad: " + edad + " | Tipo: " + tipo);
+            root.setStyle("-fx-background-color: #45997b;");
+            lblResultado.setStyle("-fx-background-color: #707354;");
+        }
+        });
+
+        btnReset.setOnAction(e -> {
+            tfNombre.clear();
+            tfApellido.clear();
+            tfEdad.clear();
+            cbTipo.getSelectionModel().clearSelection();
+            lblResultado.setText("");
+            root.setStyle("-fx-background-color: #56394a;");
+            lblResultado.setStyle("-fx-background-color: #958aa6;");
+        });
+        Scene scene = new Scene(root, 500, 400);
+        stage.setTitle("Login");
+        Image imagen = new Image(getClass().getResourceAsStream("/Imagenes/mando.png"));
+        stage.getIcons().add(imagen);
+        stage.setScene(scene);
+        stage.show();
+
 
     }
 
