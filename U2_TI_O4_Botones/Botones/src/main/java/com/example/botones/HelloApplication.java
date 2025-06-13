66a6744a3675ca5df92d422af33edcda21d2e23b
Jorge-Moranchel <20243ds059@utez.edu.mx>
2025-06-13 08:49:41 -0600

    Ya acabe xd

--- U2_TI_O4_Botones/Botones/src/main/java/com/example/botones/HelloApplication.java
+++ U2_TI_O4_Botones/Botones/src/main/java/com/example/botones/HelloApplication.java
@@ -4,30 +4,37 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import java.io.IOException;
+import javafx.geometry.Pos;
+import javafx.geometry.Insets;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) {
+
         Label label1 = new Label("label 1");
         Label label2 = new Label("label 2");
 
         Button button1 = new Button("Aceptar");
         Button button2 = new Button("Cancelar");
 
-
         button1.setOnAction(e -> label1.setText("Aceptado"));
-        button2.setOnAction(e -> label1.setText("Cancelar"));
+        button2.setOnAction(e -> label1.setText("Cancelado"));
 
-        HBox hbox = new HBox(20, label1, label2, button1, button2);
+        HBox labelBox = new HBox(10, label1, label2);
+        labelBox.setAlignment(Pos.CENTER);
 
-        Scene scene = new Scene(hbox, 300, 100);
-        stage.setTitle("Hello World");
-        //Image image = new Image(getClass().getResourceAsStream("/Image/icons8-mando-100.png"));
-        //stage.getIcons().add(image);
+        HBox buttonBox = new HBox(20, button1, button2);
+        buttonBox.setAlignment(Pos.CENTER);
+
+        VBox root = new VBox(20, labelBox, buttonBox);
+        root.setAlignment(Pos.CENTER);
+        root.setPadding(new Insets(20));
+
+        Scene scene = new Scene(root, 400, 200);
+        stage.setTitle("Interfaz xd");
         stage.setScene(scene);
         stage.show();
     }
