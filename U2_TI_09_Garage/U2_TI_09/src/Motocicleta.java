1d24bfa60723c191d19e879440b0956eb55d56c1
Jorge-Moranchel <20243ds059@utez.edu.mx>
2025-06-18 08:17:45 -0600

    Ya quedaron los otros archivos ahorita ando atorado con esta vaina pero bueno

---
+++ U2_TI_09_Garage/U2_TI_09/src/Motocicleta.java
@@ -0,0 +1,9 @@
+public class Motocicleta extends Vehiculo {
+
+    private boolean tieneCaja;
+
+    public Motocicleta(String placa, String marca, int numPuertas) {
+        super(placa, marca);
+        this.tieneCaja = tieneCaja;
+    }
+}
