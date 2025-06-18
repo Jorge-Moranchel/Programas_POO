4a5a3c8646b7d6fded443182177b81e894ccf0f0
Jorge-Moranchel <20243ds059@utez.edu.mx>
2025-06-17 21:05:31 -0600

    Mi ultimo git commit asi como le hace el Uriel a su repositorio xd

--- U2_TI_08_Empleado/U2_TI_08/src/EmpleadoTiempoCompleto.java
+++ U2_TI_08_Empleado/U2_TI_08/src/EmpleadoTiempoCompleto.java
@@ -1,2 +1,15 @@
-public class EmpleadoTiempoCompleto {
+public class EmpleadoTiempoCompleto extends Empleado {
+    private String beneficios;
+
+    public EmpleadoTiempoCompleto(String id, String nombre, double salarioBase, String beneficios) {
+        super(id, nombre, salarioBase);
+        this.beneficios = beneficios;
+    }
+    @Override
+    public double calcularSalario() {
+        return getSalarioBase();
+    }
+    public String getBeneficios() {
+        return beneficios;
+    }
 }
--- U2_TI_08_Empleado/U2_TI_08/src/Main.java
+++ U2_TI_08_Empleado/U2_TI_08/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        EmpleadoTiempoCompleto empleadoCompleto = new EmpleadoTiempoCompleto("JEMN1818", "Jorge Emmanuel", 3000.0, "Seguro Médico y Dental este es premium mucho beneficio");
+        System.out.println("Salario de " + empleadoCompleto.getNombre() + " (Tiempo Completo): $" + empleadoCompleto.calcularSalario());
 
+        EmpleadoPorHoras empleadoPorHoras = new EmpleadoPorHoras("JEMN1818", "Jorge Mora", 2500.0, 24);
+        System.out.println("Salario de " + empleadoPorHoras.getNombre() + " (Por Horas): $" + empleadoPorHoras.calcularSalario());
     }
 }
