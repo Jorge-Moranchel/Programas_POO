220750d6b6e441ace25cbb4229d69e42d4419272
Jorge-Moranchel <20243ds059@utez.edu.mx>
2025-06-17 20:54:21 -0600

    Termine el de empleado con puro autocompletado de IntellijIdea pero bueno ya quedo supongo y hice gets and sets

---
+++ U2_TI_08_Empleado/U2_TI_08/src/Empleado.java
@@ -0,0 +1,30 @@
+public abstract class Empleado {
+    private String id;
+    private String nombre;
+    private double salarioBase;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getSalarioBase() {
+        return salarioBase;
+    }
+
+    public void setSalarioBase(double salarioBase) {
+        this.salarioBase = salarioBase;
+    }
+    public abstract double calcularSalario();
+}
